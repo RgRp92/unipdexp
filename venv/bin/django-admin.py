48120dbe0ab1fec7e86607467070275b89d2ea6d
Ruggiero Rippo <ruggiero.rippo@unitn.it>
2021-10-22 12:28:35 +0200

    clear

--- venv/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Users/Ruggiero/Library/Mobile Documents/com~apple~CloudDocs/oTree/PadovaExp/venv/bin/python
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
